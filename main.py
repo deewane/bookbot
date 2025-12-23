96726073f62856ceca5e0be7b23eb22540ca1102
deewane <digjr1031@gmail.com>
2025-12-22 23:25:23 -0800

    adding logic

--- main.py
+++ main.py
@@ -13,6 +13,18 @@ def get_num_words():
     wordcount = len(words)
     print(f"Found {wordcount} total words")
     
+def samewords():
+    text = get_book_text("books/frankenstein.txt")
+    word_to_find = input("type in the word you want to find")
+    words = text.split()
+    num_word = 0
+    for word in words:
+        if word == word_to_find:
+            num_word += 1
+    if num_word == 0:
+        print("word not found")
+    else:
+        print(f"the word {word_to_find} was found a number of {num_word}")
 
 
-get_num_words()
+samewords()
