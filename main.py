d46c9312337e2c1f2ed278b31fe093901da1fb1f
deewane <digjr1031@gmail.com>
2025-12-22 22:49:35 -0800

    change to code

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath, "r", encoding="utf-8") as f:
+        return f.read()
+
+
+def main():
+    book_text = get_book_text("books/frankenstein.txt")
+    print(book_text)
+
+def get_num_words():
+    text = get_book_text("books/frankenstein.txt")
+    words = text.split()
+    print(len(words))
+    
+
+
+get_num_words()
